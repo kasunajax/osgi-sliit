432ccd9dda6c4859144fe04fb1ef1601f4a6f63b
Sanomikabandara <36528193+Sanomikabandara@users.noreply.github.com>
2019-03-21 11:25:01 +0530

    day2

--- RoomManagementPublisher/src/roommanagementpublisher/Activator.java
+++ RoomManagementPublisher/src/roommanagementpublisher/Activator.java
@@ -2,10 +2,12 @@ package roommanagementpublisher;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	ServiceRegistration registration;
 
 	static BundleContext getContext() {
 		return context;
@@ -13,10 +15,15 @@ public class Activator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		Activator.context = bundleContext;
+		System.out.println("Starting Room management service");
+		registration = bundleContext.registerService(RoomManagementPublish.class.getName(), 
+				new RoomManageImpl(), null);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		System.out.println("Stopping Room management service");
+		registration.unregister();
 	}
 
 }
--- RoomManagementPublisher/src/roommanagementpublisher/RoomManageImpl.java
+++ RoomManagementPublisher/src/roommanagementpublisher/RoomManageImpl.java
@@ -1,5 +1,47 @@
 package roommanagementpublisher;
 
-public class RoomManageImpl {
+public class RoomManageImpl implements RoomManagementPublish{
+
+	@Override
+	public void addRoomDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRoomDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRoomStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkAvailability() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DisplayRoomDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeleteRoomDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void searchRooms() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
